29b9a6c0defc841f17b063ad3d52cb0667448a0b
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-04-02 17:48:56 -0600

    ¿Qué se implemento en ES7?

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+//includes
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(9)) {
+  console.log('si se encuentra el valor 7');
+} else {
+  console.log('no se encuentra');
+}
+
+//exponential operator
+let base = 4;
+let exponente = 3;
+let resultado = base ** exponente;
+
+console.log(resultado);
