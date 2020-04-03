cf36d11d77348c9de0936aba0ef3dc037152e0d7
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-04-02 18:12:26 -0600

    Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -19,3 +19,23 @@ console.log(hello.padEnd(12, '---'));
 const obj = {
   name: 'juan' //esta coma del final indica que puede o no existir otro elemento después
 };
+
+// async y await
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve('Hello World'), 3000)
+      : reject(new Error('Test Error'));
+  });
+};
+
+const helloAsync = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+helloAsync();
